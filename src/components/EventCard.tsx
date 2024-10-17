e8ffb92966743dcb39bf5e61bb94f29a1e19c2a4
Erick Ruiz <erickuriel238@gmail.com>
2024-10-16 19:03:42 -0600

    Refactor EventCard component to update card styling and add more info button

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -76,9 +76,9 @@ const EventCard: React.FC<EventCardProps> = ({ events }) => {
             // Card Styling
             <div
               key={event.id}
-              className="min-w-[260px] max-w-[260px] lg:min-w-[325px] lg:max-w-[325px] h-[360px] lg:h-[400px]
-              flex flex-col items-center justify-center shadow-md
-             rounded-xl bg-gradient-to-br from-indigo-300 to-white dark:to-black"
+              className="min-w-[260px] max-w-[260px] lg:min-w-[325px] lg:max-w-[325px] h-[360px] lg:h-[420px]
+              flex flex-col items-center justify-center shadow-md rounded-xl
+               bg-gradient-to-br from-indigo-300 to-white dark:to-black"
             >
               {event.images && event.images[0] && (
                 <img
@@ -87,33 +87,31 @@ const EventCard: React.FC<EventCardProps> = ({ events }) => {
                   alt={event.name}
                 />
               )}
-              <div className="px-5 py-1 lg:py-2 w-full">
-                <div className="font-bold text-base lg:text-lg mb-1 text-center text-blue-600 dark:text-white">
+              <div className="px-5 py-1 lg:py-2 w-full text-neutral-700 dark:text-white text-sm text-center font-semibold">
+                <div className="font-extrabold text-base lg:text-lg mb-1 text-center text-blue-600 dark:text-white">
                   {event.name}
                 </div>
-                <p className="text-gray-700 dark:text-white text-sm text-center">
-                  Date: <span>{formattedDate}</span>
+                <p>
+                  <span>Date:</span> {formattedDate}
                 </p>
                 {event._embedded?.venues &&
                   event._embedded.venues.length > 0 && (
-                    <p className="text-gray-700 dark:text-white text-sm text-center">
-                      Venue: <span>{event._embedded.venues[0].name}</span>
+                    <p>
+                      <span>Venue:</span> {event._embedded.venues[0].name}
                     </p>
                   )}
                 {event.priceRanges && event.priceRanges.length > 0 && (
-                  <p className="text-gray-700 dark:text-white text-sm text-center">
-                    <span>
-                      Price:{" "}
-                      {event.priceRanges && event.priceRanges.length > 0 ? (
-                        <>
-                          ${event.priceRanges[0].min} - $
-                          {event.priceRanges[0].max}{" "}
-                          {event.priceRanges[0].currency}
-                        </>
-                      ) : (
-                        <span>Price not available</span>
-                      )}
-                    </span>
+                  <p>
+                    <span>Price: </span>
+                    {event.priceRanges && event.priceRanges.length > 0 ? (
+                      <>
+                        ${event.priceRanges[0].min} - $
+                        {event.priceRanges[0].max}{" "}
+                        {event.priceRanges[0].currency}
+                      </>
+                    ) : (
+                      <span>Price not available</span>
+                    )}
                   </p>
                 )}
                 <button className="flex justify-end my-3 w-full">
@@ -122,7 +120,7 @@ const EventCard: React.FC<EventCardProps> = ({ events }) => {
                       href={event.url}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-sm border-2 border-blue-600 text-blue-600 px-4 py-2 rounded-md
+                      className="text-sm font-bold border-2 border-blue-600 text-blue-600 px-3 py-2 rounded-md
                         hover:bg-blue-600 hover:text-white transition-colors duration-200"
                     >
                       More info
@@ -134,8 +132,8 @@ const EventCard: React.FC<EventCardProps> = ({ events }) => {
           );
         })}
       </div>
-        
-        {/* Arrow Buttons */}
+
+      {/* Arrow Buttons */}
       <button id="arrow" className="left-5" onClick={() => scroll("left")}>
         <svg
           className="w-4 h-4"

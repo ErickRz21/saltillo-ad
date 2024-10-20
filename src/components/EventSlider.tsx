f20a50c47ced80b2d9d2c05f41a80c8890aef248
ErickRz21 <erickuriel238@gmail.com>
2024-10-19 23:31:59 -0600

    Refactor EventSlider component to remove unused import

--- src/components/EventSlider.tsx
+++ src/components/EventSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { EventType } from "../types/EventType";
 import EventCard from "./EventCard";
 import useCarouselScroll from "../hooks/useSliderScroll";

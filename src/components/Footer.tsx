f5a81badcf9f9d2bc6c302f681fba89dd4d90fd2
ErickRz21 <erickuriel238@gmail.com>
2024-11-27 20:22:07 -0600

    Update Footer component; adjust text size for improved readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaGithub, FaInstagram } from "react-icons/fa";
 const Footer: React.FC = () => {
   return (
     <footer className="bottom-0 relative border-t-2 border-neutral-300 dark:border-neutral-700 my-14 lg:my-20 pb-10 lg:pb-0 items-center">
-      <div className="flex flex-col lg:items-center lg:flex-row justify-evenly text-base lg:text-xl font-bold my-6 lg:my-16 mx-auto space-x-0 space-y-3 lg:space-y-0">
+      <div className="flex flex-col lg:items-center lg:flex-row justify-evenly text-base lg:text-lg font-bold my-6 lg:my-16 mx-auto space-x-0 space-y-3 lg:space-y-0">
         <p className="text-neutral-500">
           &copy; Event. {new Date().getFullYear()} All rights reserved.
         </p>
